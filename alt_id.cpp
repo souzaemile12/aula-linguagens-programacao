6cd629cb6e0947ba3c0534b69294885efaa2dc49
Emile Souza <107773161+souzaemile12@users.noreply.github.com>
2024-04-05 21:08:13 -0300

    Exibe idade e altura.

---
+++ alt_id.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade;
+    float altura;
+    cin >> idade;
+    cout << "A idade é:"<< idade << endl;
+    cin >> altura;
+    cout << "A altura é:" << altura << endl;
+    return 0;
+
+}
